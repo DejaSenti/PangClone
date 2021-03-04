8b0107b7c17fddc5f65331d890271b21295edef1
tomlyden <tom.lyden.zzz@gmail.com>
2021-03-04 14:17:35 +0200

    Added player spawn points to level Some folder ordering

---
+++ Assets/Scripts/Player/PlayerID.cs
@@ -0,0 +1,6 @@
+﻿public enum PlayerID
+{
+    None,
+    Player1,
+    Player2
+}
---
+++ Assets/Scripts/SpawnPoints/PlayerSpawnPoint.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class PlayerSpawnPoint : MonoBehaviour
+{
+    public PlayerID PlayerID;
+}

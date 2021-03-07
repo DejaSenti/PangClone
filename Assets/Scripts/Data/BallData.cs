40b963a6ee1b935687ef23140df32202d3d73264
tomlyden <tom.lyden.zzz@gmail.com>
2021-03-07 13:41:05 +0200

    Fixed compilation error

--- Assets/Scripts/Data/BallData.cs
+++ Assets/Scripts/Data/BallData.cs
@@ -4,7 +4,7 @@ public static class BallData
 {
     public const int NUM_SPLIT_BALLS = 2;
     // The array MUST be the same size as the integer!
-    public const Vector2[] DIRECTION_VECTORS = new Vector2[] { new Vector2(1, 1), new Vector2(-1, 1) };
+    public static readonly Vector2[] DIRECTION_VECTORS = new Vector2[] { new Vector2(1, 1), new Vector2(-1, 1) };
            
     public const float BALL_SPEED_ON_SPAWN = 3;
 }
